001c46484bdf6fb66d4e43af101d79d0596dfa69
Sebastian Heiden <heiden-sebastian@t-online.de>
2024-09-22 01:50:28 +0200

    rewrite

--- schiffsrouten/download.py
+++ schiffsrouten/download.py
@@ -1,35 +1,28 @@
 """Load the Raster selected via json file."""
 
 import json
-from datetime import datetime
+from dataclasses import dataclass
 from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
 import requests
 from shapely.geometry import Polygon, shape
-from shapely.geometry.polygon import Polygon
-
-with Path("../config/download.json").open() as f:
-    config = json.load(f)
-
-copernicus_user = config["copernicus_user"]  # copernicus User
-copernicus_password = config["copernicus_password"]  # copernicus Password
-ft = Polygon(config["area"])  # WKT Representation of BBOX
-data_collection = config["data_collection"]  # Sentinel satellite
-
-start_date_string = config["start_date"]
-start_date = datetime.strptime(start_date_string, "%Y-%m-%d").date
-end_date_string = config["end_date"]
-end_date = datetime.strptime(end_date_string, "%Y-%m-%d").date()
-attributes = config["attributes"]
+from tqdm import tqdm
 
 
-def get_keycloak(username: str, password: str) -> str:
+@dataclass
+class UserData:
+    name: str
+    password: str
+
+
+def get_keycloak(user: UserData) -> str:
+    """Lock up a bearer token."""
     data = {
         "client_id": "cdse-public",
-        "username": username,
-        "password": password,
+        "username": user.name,
+        "password": user.password,
         "grant_type": "password",
     }
     try:
@@ -44,48 +37,82 @@ def get_keycloak(username: str, password: str) -> str:
         raise AttributeError(error_txt) from None
     return r.json()["access_token"]
 
-add_attributes = ""
-#if len(attributes):
-#    for name, value in attributes.items():
-#        add_attributes += f" and Attributes/OData.StringAttribute/any(att:att/Name eq '{name}' and att/OData.StringAttribute/Value eq '{value}')"
 
+@dataclass
+class Period:
+    """Period to search in."""
+
+    start: str
+    end: str
+
+
+def get_search_string(collection: str, area: Polygon, period: Period, attributes: dict[str, str]) -> str:
+    """Build a search string for a given collection and area."""
+    add_attributes = ""
+    if len(attributes):
+        for name, value in attributes.items():
+            add_attributes += f" and Attributes/OData.CSC.StringAttribute/any(att:att/Name eq '{name}' and att/OData.CSC.StringAttribute/Value eq '{value}')"
+
+    base_query = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{collection}' and OData.CSC.Intersects(area=geography'SRID=4326;{area}') and ContentDate/Start gt {period.start}T00:00:00.000Z and ContentDate/Start lt {period.end}T00:00:00.000Z"
+    final_query = "&$count=True&$top=1000&$expand=Attributes"
+    return base_query + add_attributes + final_query
+
+
+def to_geo(data: pd.DataFrame) -> gpd.GeoDataFrame:
+    """Convert the DataFrame to usable GeoDataFrame."""
+    local_df = data.copy()
+    return (
+        local_df.assign(geometry=lambda x: x.GeoFootprint.apply(shape))
+        .set_geometry("geometry")
+        .assign(identifier=lambda x: x.Name.str.split(".").str[0])
+        .loc[lambda x: ~x["Name"].str.contains("L1C")]
+    )
+
+
+def download_raster(properties: dict, token: str, save_dir: Path) -> None:
+    """Download raster using the keycloak token."""
+    session = requests.Session()
+
+    session.headers.update({"Authorization": f"Bearer {token}"})
+    url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products({properties['Id']})/$value"
+    response = session.get(url, allow_redirects=False)
+    while response.status_code in (301, 302, 303, 307):
+        url = response.headers["Location"]
+        response = session.get(url, allow_redirects=False)
+
+    file = session.get(url, verify=False, allow_redirects=True)
+
+    with (save_dir / f"{properties['identifier']}.zip").open("wb") as f:
+        f.write(file.content)
+
+
+with Path("../config/download.json").open() as f:
+    config = json.load(f)
 
 json_ = requests.get(
-    f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name eq '{data_collection}' and OData.CSC.Intersects(area=geography'SRID=4326;{ft}') and ContentDate/Start gt {start_date_string}T00:00:00.000Z and ContentDate/Start lt {end_date_string}T00:00:00.000Z&$count=True&$top=1000"
+    get_search_string(
+        collection=config["data_collection"],
+        area=Polygon(config["area"]),
+        period=Period(**config["period"]),
+        attributes=config["attributes"]
+    ),
+    timeout=10,
 ).json()
 p_df = pd.DataFrame.from_dict(json_["value"])  # Fetch available dataset
-if p_df.shape[0] > 0:
-    p_df["geometry"] = p_df["GeoFootprint"].apply(shape)
-    p_gdf = gpd.GeoDataFrame(p_df).set_geometry("geometry")  # Convert PD to GPD
-    p_gdf = p_gdf[~p_gdf["Name"].str.contains("L1C")]  # Remove L1C dataset
-    print(f" total tiles found {len(p_gdf)}")
-    p_gdf["identifier"] = p_gdf["Name"].str.split(".").str[0]
-    num_samples = len(p_gdf)
+if p_df.shape[0] == 0:
+    error_txt = "No data found"
+    raise AttributeError(error_txt)
 
-    if num_samples == 0:
-        print("No tiles found for today")
-    else:
-        ## download all tiles from server
-        for index, feat in enumerate(p_gdf.iterfeatures()):
-            try:
-                session = requests.Session()
-                keycloak_token = get_keycloak(copernicus_user, copernicus_password)
-                session.headers.update({"Authorization": f"Bearer {keycloak_token}"})
-                url = f"https://catalogue.dataspace.copernicus.eu/odata/v1/Products({feat['properties']['Id']})/$value"
-                response = session.get(url, allow_redirects=False)
-                while response.status_code in (301, 302, 303, 307):
-                    url = response.headers["Location"]
-                    response = session.get(url, allow_redirects=False)
-                print(feat["properties"]["Id"])
-                file = session.get(url, verify=False, allow_redirects=True)
+p_gdf = to_geo(p_df)
+num_samples = len(p_gdf)
+if num_samples == 0:
+    error_txt = "No tiles found for given period"
+    raise AttributeError(error_txt)
 
-                with open(
-                    f"{feat['properties']['identifier']}.zip",  # location to save zip from copernicus
-                    "wb",
-                ) as p_df:
-                    print(feat["properties"]["Name"])
-                    p_df.write(file.content)
-            except:
-                print("problem with server")
-else:
-    print("no data found")
+## download all tiles from server
+token = get_keycloak(UserData(**config["user"]))
+for feat in tqdm(p_gdf.iterfeatures()):
+    try:
+        download_raster(feat["properties"],token , Path(config["save_dir"]))
+    except:
+        print("problem with server")
